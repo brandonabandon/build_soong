454fae24c667384b9b0d90738c4e058866a51f29
mydongistiny <jaysonedson@gmail.com>
2018-02-07 15:47:38 -0800

    clang: Setup for DragonTC and eightone [2/2]

--- ui/build/make.go
+++ ui/build/make.go
@@ -86,6 +86,9 @@ func runMakeProductConfig(ctx Context, config Config) {
 		"TARGET_BUILD_VARIANT",
 		"TARGET_BUILD_APPS",
 
+		"CLANG",
+		"CLANG_CXX",
+
 		// compiler wrappers set up by make
 		"CC_WRAPPER",
 		"CXX_WRAPPER",
